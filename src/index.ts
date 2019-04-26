9b1e4fa65c8b0ec4e3cfe106a1629185500e8ea5
josh <postor@gmail.com>
2019-04-26 20:54:21 +0800

    test

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,23 @@
-require('cesium/Build/Cesium/Widgets/widgets.css')
-require('./css/main.css')
-import * as Cesium from 'cesium'
-console.log(Cesium)
+require('cesium/Build/Cesium/Widgets/widgets.css');
+require('./css/main.css');
+require('./heatmap.js')
+import * as Cesium from 'cesium';
+import * as h337 from 'heatmap.js'
 
-var viewer = new Cesium.Viewer('container');
+
+
+const viewer = new Cesium.Viewer('container');
+const scene = viewer.scene;
+const camera = scene.camera;
+
+let bounds = {
+  west: 147.13833844,
+  east: 147.13856899,
+  south: -41.43606916,
+  north: -41.43582929
+};
+
+// @ts-ignore
+//viewer.container = document.getElementById('container')
+console.log(viewer.container)
+// init heatmap
---
+++ src/lib/Heat.ts
@@ -0,0 +1,5 @@
+class Heat {
+  constructor(public position: Cesium.Cartesian3, public value: number = 1,) { }
+}
+
+export {Heat} ;
